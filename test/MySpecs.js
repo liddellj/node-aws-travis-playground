aab883da6ff0eeb751fa6d822481f6845bb709fe
Jim Liddell <jim@liddellj.com>
2015-08-20 17:03:55 +0100

    package.json tweaks.

--- test/MySpecs.js
+++ test/MySpecs.js
@@ -2,7 +2,7 @@ var should = require('chai').should();
 var app = require('../src/server');
 
 describe('MyApp', function() {
-	it('should be testable', function() {
-		true.should.equal(true);
-	});
+    it('should be testable', function() {
+        true.should.equal(true);
+    });
 });
