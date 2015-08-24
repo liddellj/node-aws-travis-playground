9e857623b9bf4a8368fd8406345eb490500c5f8f
Jim Liddell <jim@liddellj.com>
2015-08-24 13:49:17 +0100

    Fixed issue with quoting when invoking sed.

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default class App extends React.Component {
     render() {
         return <div>
-            <h1>Hello Jim's App Handler Version 4.1.0</h1>
+            <h1>Hello Jim's App Handler Version 4.1.1</h1>
             <RouteHandler />
         </div>;
     }
