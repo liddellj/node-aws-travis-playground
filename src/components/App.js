c4dc70505acabc7679f7f79d95dc61c628f29dbc
Jim Liddell <jim@liddellj.com>
2015-08-24 15:51:54 +0100

    Blah.

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default class App extends React.Component {
     render() {
         return <div>
-            <h1>Hello Jim's App Handler Version 4.1.2</h1>
+            <h1>Hello Jim's App Handler Version {process.env.npm_package_version}</h1>
             <RouteHandler />
         </div>;
     }
