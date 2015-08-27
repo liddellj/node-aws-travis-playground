e4f89073aaa502a6490e10f635721a0af563a524
Jim Liddell <jim@liddellj.com>
2015-08-27 10:39:15 +0100

    Switched out request for supertest.

--- test/acceptance/AcceptanceSpec.js
+++ test/acceptance/AcceptanceSpec.js
@@ -1,32 +1,15 @@
 import chai from 'chai';
-import request from 'request';
+import request from 'supertest';
 import app from '../../src/server';
 const should = chai.should();
 
 describe('My Acceptance Test', function() {
-  let server;
-
-  beforeEach(function(done) {
-    server = app.listen(3000, function() {
-      done();
-    });
-  });
-
   it('should return 200', function(done) {
-    request.get('http://localhost:3000/dynamodb', function (err, res, body) {
-      if (err) {
-        console.log(err, err.stack);
-      } else {
-        res.statusCode.should.equal(200);
-        res.body.should.not.be.null;
-        JSON.parse(res.body).should.eql({ id: 666, name: 'Garfield' });
-      }
-
-      done(err);
-    });
-  });
-
-  afterEach(function() {
-    server.close();
+    request(app)
+      .get('/dynamodb')
+      .expect(function(res) {
+        res.body.should.eql({ id: 666, name: 'Garfield' });
+      })
+      .expect(200, done);
   });
 });
