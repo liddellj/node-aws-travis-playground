ed8cc25376b9530f9c0dd658c9d637a4a226764b
Jim Liddell <jim@liddellj.com>
2015-09-16 15:23:38 +0100

    Fixes.

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,8 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     profileId: DataTypes.STRING,
     name: DataTypes.STRING
+  },{
+    timestamps: false
   });
 
   return User;
